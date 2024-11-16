38c705955078c04cbbdec57b86b8641bf0e96a7b
Priya-Sivalingam <priyasivalingam99@gmail.com>
2024-11-16 18:55:17 +0530

    Update App.js file

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import EmployeeList from './components/EmployeeList';
+import EmployeeDetails from './components/EmployeeDetails';
+import AddEmployee from './components/AddEmployee';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route path="/login" component={Login} />
+                    <Route path="/employees" component={EmployeeList} />
+                    <Route path="/employee/:id" component={EmployeeDetails} />
+                    <Route path="/add-employee" component={AddEmployee} />
+                </Switch>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
